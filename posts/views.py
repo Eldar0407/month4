5de1b6eecb13c025babc925bc7fb30b8e53e42f1
Eldar0407 <abiev.eldar_11@mail.ru>
2024-10-22 12:58:44 +0600

    2

--- posts/views.py
+++ posts/views.py
@@ -1,10 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from posts.models import Post
 import random
 
 
+
 def test_view(request):
-    return HttpResponse(random.randint(1,100))
+    return HttpResponse(random.randint(1, 100))
+
 
 def main_page_view(request):
-    return render(request, 'base.html')
+    return render(request,  'base.html')
+
+
+def post_list_view(request):
+    posts = Post.objects.all()
+    return render(request, "post_list.html", context={"posts": posts})
+
+
+def post_detail_view(request, post_id):
+    post = Post.objects.get(id=post_id)
+    return render(request, "post_detail.html", context={"post": post})
